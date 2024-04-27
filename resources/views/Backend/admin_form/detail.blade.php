f82cd263b296c33b51e6e7adbb66b73640851bf4
Gretawdm <gretadhita58@gmail.com>
2024-04-28 00:19:17 +0700

    update form detail admin

--- resources/views/Backend/admin_form/detail.blade.php
+++ resources/views/Backend/admin_form/detail.blade.php
@@ -1,105 +1,110 @@
 @extends('backend.app')
 @section('content')
     <div class="container-fluid">
-        <h1 class="h3 mb-2" style="color: black">Detail Event</h1>
-        <div class="row">
-            <div class="col-lg-6">
+        <h1 class="h3 mb-3" style="color: black">Detail Pendaftaran Event</h1>
+        <div class="card mb-5 shadow" style="padding: 2% 0% 0% 5%;">
+            <div class="row">
+                <div class="col-lg-6">
+                    <!-- General Form Elements -->
+                    <form>
+                        <div class="row mb-3">
+                            <div class="col-sm-4 col-form-label">Poster :</div>
+                            <div class="col-sm-10">
+                                <img src="/image/{{ $detailevent['poster'] }}" alt="Poster" style="max-width: 300px" />
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col-sm-4 col-form-label">Nama Event :</div>
+                            <div class="col-sm-10">
+                                {{ $detailevent->nama_event }}
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col-sm-4 col-form-label">Deskripsi Event :</div>
+                            <div class="col-sm-10">
+                                {{ $detailevent->deskripsi_event }}
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col-sm-4 col-form-label">Contact Person :</div>
+                            <div class="col-sm-10">
+                                {{ $detailevent->contact_person }}
+                            </div>
+                        </div>
+                    </form>
+                    <!-- End General Form Elements -->
+                </div>
 
-                <!-- General Form Elements -->
-                <form>
-                    <div class="row mb-3">
-                        <div class="col-sm-4 col-form-label">Poster :</div>
-                        <div class="col-sm-10">
-                            <img src="/image/{{ $detailevent['poster'] }}" alt="Poster" style="max-width: 300px" />
+                <div class="col-lg-6">
+                    <form>
+                        <div class="row mb-3">
+                            <div class="col-sm-4 col-form-label">Event Organizer :</div>
+                            <div class="col-sm-10">
+                                {{ $detailevent->event_organizer }}
+                            </div>
                         </div>
-                    </div>
-                    <div class="row mb-3">
-                        <div class="col-sm-4 col-form-label">Nama Event :</div>
-                        <div class="col-sm-10">
-                            {{ $detailevent->nama_event }}
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <div class="col-sm-4 col-form-label">Deskripsi Event :</div>
-                        <div class="col-sm-10">
-                            {{ $detailevent->deskripsi_event }}
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <div class="col-sm-4 col-form-label">Contact Person :</div>
-                        <div class="col-sm-10">
-                            {{ $detailevent->contact_person }}
-                        </div>
-                    </div>
-                </form>
-                <!-- End General Form Elements -->
-            </div>
-
-            <div class="col-lg-6">
-                <form>
-                    <div class="row mb-3">
-                        <div class="col-sm-4 col-form-label">Event Organizer :</div>
-                        <div class="col-sm-10">
-                            {{ $detailevent->event_organizer }}
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                        <div class="col-sm-4 col-form-label">Event Owner</label>
+                        <div class="row mb-3">
+                            <div class="col-sm-4 col-form-label">Event Owner :</div>
                             <div class="col-sm-10">
                                 {{ $detailevent->event_owner }}
                             </div>
                         </div>
+
+
                         <div class="row mb-3">
-                            <div class="col-sm-4 col-form-label">Tanggal Event</div>
+                            <div class="col-sm-4 col-form-label">Tanggal Event :</div>
                             <div class="col-sm-10">
                                 {{ $detailevent->tanggal_event }}
                             </div>
                         </div>
                         <div class="row mb-3">
-                            <div class="col-sm-4 col-form-label">No Rekening</div>
+                            <div class="col-sm-4 col-form-label">No Rekening :</div>
                             <div class="col-sm-10">
                                 {{ $detailevent->no_rek }}
                             </div>
                         </div>
                         <div class="row mb-3">
-                            <div class="col-sm-4 col-form-label">Nama Rekening</div>
+                            <div class="col-sm-4 col-form-label">Nama Rekening :</div>
                             <div class="col-sm-10">
                                 {{ $detailevent->nama_rekening }}
                             </div>
                         </div>
                         <div class="row mb-3">
-                            <div class="col-sm-4 col-form-label">Alamat Event</label>
-                                <div class="col-sm-10">
-                                    {{ $detailevent->alamat_event }}
-                                </div>
+                            <div class="col-sm-4 col-form-label">Alamat Event :</div>
+                            <div class="col-sm-10">
+                                {{ $detailevent->alamat_event }}
                             </div>
                         </div>
+
                         <div class="row mb-3">
-                            <div class="col-sm-4 col-form-label">Tanggal Pendaftaran Booth Tenant</label>
-                                <div class="col-sm-10">
-                                    {{ $detailevent->tanggal_pendaftaran_booth_tenant }}
-                                </div>
+                            <div class="col-sm-4 col-form-label">Tanggal Pendaftaran Booth Tenant :</div>
+                            <div class="col-sm-10">
+                                {{ $detailevent->tanggal_pendaftaran_booth_tenant }}
                             </div>
                         </div>
+
                         <div class="row mb-3">
-                            <div class="col-sm-4 col-form-label">Booth Yang Tersedia</label>
-                                <div class="col-sm-10">
-                                    {{ $detailevent->booth }}
-                                </div>
+                            <div class="col-sm-4 col-form-label">Booth Yang Tersedia :</div>
+                            <div class="col-sm-10">
+                                {{ $detailevent->booth }}
                             </div>
                         </div>
+
                         <div class="row mb-3">
-                            <div class="col-sm-4 col-form-label">Denah Event</label>
-                                <div class="col-sm-10">
-                                    {{ $detailevent->denah }}
-                                </div>
+                            <div class="col-sm-4 col-form-label">Denah Event :</div>
+                            <div class="col-sm-10">
+                                <img src="/image/{{ $detailevent['denah'] }}" alt="Poster" style="max-width: 300px" />
                             </div>
                         </div>
+                </div>
                 </form>
                 <!-- End General Form Elements -->
             </div>
+
         </div>
-        </section>
-        </main>
-        <!-- End #main -->
-    @endsection
+
+    </div>
+    </section>
+    </main>
+    <!-- End #main -->
+@endsection
